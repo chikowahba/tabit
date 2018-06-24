fa035651c5713e738f92ba5919ebebfe9faf733a
moshe wahba <chikowahba2@gmail.com>
2018-06-24 13:18:32 +0300

    add http server

--- src/modules/home/home.js
+++ src/modules/home/home.js
@@ -1,8 +1,9 @@
 var home = null
 rosApp.controller('HomeController',
         function($scope, $rootScope,catFactory,modifiersFactory) {
-            
+
             home = $scope;
+            $scope.searchText = '';
             $scope.tabsParam = 'menu';
             $scope.categories = angular.copy(catFactory.getCategories());
             $scope.modifiers_groups = angular.copy(modifiersFactory.getModifiersGroup());
@@ -10,6 +11,7 @@ rosApp.controller('HomeController',
 
             $scope.changeTab = function (cat) {
                 $scope.tabsParam = cat;
+                $scope.searchText = '';
             }
         }
 );
